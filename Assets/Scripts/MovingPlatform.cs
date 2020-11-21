4d493a4cfc7942a84b655c1f97dd3b41d26dee3c
krthush <33790334+krthush@users.noreply.github.com>
2020-11-21 13:08:32 +0000

    passenger mover working

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -1,10 +1,7 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 
-public class MovingPlatform : ColliderCasts
+public class MovingPlatform : MonoBehaviour
 {
-
-	public LayerMask passengerMask;
 
 	public Vector3[] localWaypoints;
 	Vector3[] globalWaypoints;
@@ -19,33 +16,39 @@ public class MovingPlatform : ColliderCasts
 	float percentBetweenWaypoints;
 	float nextMoveTime;
 
-	List<PassengerMovement> passengerMovement;
-	Dictionary<Transform, Passenger> passengerDictionary = new Dictionary<Transform, Passenger>();
+	PassengerMover passengerMover;
 
-	public override void Start()
+	void Start()
 	{
-		base.Start();
 
 		globalWaypoints = new Vector3[localWaypoints.Length];
 		for (int i = 0; i < localWaypoints.Length; i++)
 		{
 			globalWaypoints[i] = localWaypoints[i] + transform.position;
 		}
+
+		passengerMover = GetComponent<PassengerMover>();
 	}
 
 	void Update()
 	{
-		UpdateBoxCastOrigins();
 
 		if (globalWaypoints.Length > 0)
 		{
 			Vector3 displacement = CalculatePlatformMovement();
 
-			CalculatePassengerMovement(displacement);
+			if (passengerMover)
+			{
+				passengerMover.CalculatePassengerMovement(displacement);
 
-			MovePassengers(true);
-			transform.Translate(displacement);
-			MovePassengers(false);
+				passengerMover.MovePassengers(true);
+				transform.Translate(displacement);
+				passengerMover.MovePassengers(false);
+
+			} else
+			{
+				transform.Translate(displacement);
+			}
 		}
 	}
 
@@ -89,133 +92,6 @@ public class MovingPlatform : ColliderCasts
 		}
 
 		return newPos - transform.position;
-	}
-
-	void MovePassengers(bool beforeMovePlatform)
-	{
-		foreach (PassengerMovement passenger in passengerMovement)
-		{
-			if (!passengerDictionary.ContainsKey(passenger.transform))
-			{
-				passengerDictionary.Add(passenger.transform, passenger.transform.GetComponent<Passenger>());
-			}
-
-			if (passenger.moveBeforePlatform == beforeMovePlatform)
-			{
-				passengerDictionary[passenger.transform].Move(passenger.displacement, passenger.standingOnPlatform);
-			}
-		}
-	}
-
-	void CalculatePassengerMovement(Vector3 displacement)
-	{
-		HashSet<Transform> movedPassengers = new HashSet<Transform>();
-		passengerMovement = new List<PassengerMovement>();
-
-		float directionX = Mathf.Sign(displacement.x);
-		float directionY = Mathf.Sign(displacement.y);
-
-        // Vertically moving platform
-        if (displacement.y != 0)
-        {
-            float rayLength = Mathf.Abs(displacement.y);
-
-            Vector2 boxRayOrigin = (directionY == -1) ? boxCastOrigins.bottomCenter : boxCastOrigins.topCenter;
-            Vector2 boxCastSize = new Vector2(boundsWidth, skinWidth);
-            ContactFilter2D contactFilter2D = new ContactFilter2D();
-            contactFilter2D.SetLayerMask(passengerMask);
-            List<RaycastHit2D> results = new List<RaycastHit2D>();
-
-            Physics2D.BoxCast(boxRayOrigin, boxCastSize, 0, Vector2.up * directionY, contactFilter2D, results, rayLength);
-
-            results.ForEach(delegate (RaycastHit2D hit)
-            {
-                if (hit)
-                {
-                    if (!movedPassengers.Contains(hit.transform))
-                    {
-                        movedPassengers.Add(hit.transform);
-
-                        float pushX = (directionY == 1) ? displacement.x : 0;
-                        float pushY = displacement.y - (hit.distance) * directionY;
-
-                        passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), directionY == 1, true));
-                    }
-                }
-            });
-        }
-
-        // Horizontally moving platform
-        if (displacement.x != 0)
-        {
-            float rayLength = Mathf.Abs(displacement.x);
-
-            Vector2 boxRayOrigin = (directionX == -1) ? boxCastOrigins.leftCenter : boxCastOrigins.rightCenter;
-            Vector2 boxCastSize = new Vector2(skinWidth, boundsHeight);
-            ContactFilter2D contactFilter2D = new ContactFilter2D();
-            contactFilter2D.SetLayerMask(passengerMask);
-            List<RaycastHit2D> results = new List<RaycastHit2D>();
-
-            Physics2D.BoxCast(boxRayOrigin, boxCastSize, 0, Vector2.right * directionX, contactFilter2D, results, rayLength);
-
-            results.ForEach(delegate (RaycastHit2D hit)
-            {
-                if (hit)
-                {
-                    if (!movedPassengers.Contains(hit.transform))
-                    {
-                        movedPassengers.Add(hit.transform);
-
-                        float pushX = displacement.x - (hit.distance - skinWidth) * directionX;
-                        float pushY = -skinWidth;
-
-                        passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), false, true));
-                    }
-                }
-            });
-        }
-
-        // Passenger on top of a horizontally or downward moving platform
-        if (directionY == -1 || displacement.y == 0 && displacement.x != 0)
-        {
-            Vector2 boxCastSize = new Vector2(boundsWidth, skinWidth);
-            ContactFilter2D contactFilter2D = new ContactFilter2D();
-            contactFilter2D.SetLayerMask(passengerMask);
-            List<RaycastHit2D> results = new List<RaycastHit2D>();
-
-            Physics2D.BoxCast(boxCastOrigins.topCenter, boxCastSize, 0, Vector2.up, contactFilter2D, results, skinWidth);
-
-            results.ForEach(delegate (RaycastHit2D hit)
-            {
-                if (hit)
-                {
-                    if (!movedPassengers.Contains(hit.transform))
-                    {
-                        movedPassengers.Add(hit.transform);
-                        float pushX = displacement.x;
-                        float pushY = displacement.y;
-
-                        passengerMovement.Add(new PassengerMovement(hit.transform, new Vector3(pushX, pushY), true, false));
-                    }
-                }
-            });
-        }
-    }
-
-	struct PassengerMovement
-	{
-		public Transform transform;
-		public Vector3 displacement;
-		public bool standingOnPlatform;
-		public bool moveBeforePlatform;
-
-		public PassengerMovement(Transform _transform, Vector3 _displacement, bool _standingOnPlatform, bool _moveBeforePlatform)
-		{
-			transform = _transform;
-			displacement = _displacement;
-			standingOnPlatform = _standingOnPlatform;
-			moveBeforePlatform = _moveBeforePlatform;
-		}
 	}
 
 	void OnDrawGizmos()
--- Assets/Scripts/Passenger.cs
+++ Assets/Scripts/Passenger.cs
@@ -10,17 +10,36 @@ public class Passenger : MonoBehaviour
 	/// </summary>
 	public void Move(Vector2 displacement, bool standingOnPlatform)
 	{
-		if (gameObject.tag == "Player" || gameObject.tag == "Enemy")
+		PassengerMover passengerMover = gameObject.GetComponent<PassengerMover>();
+
+		if (passengerMover)
         {
+			passengerMover.CalculatePassengerMovement(displacement);
+
+			passengerMover.MovePassengers(true);
+			MoveTarget(displacement);
+			passengerMover.MovePassengers(false);
+		} else
+        {
+			MoveTarget(displacement);
+		}
+	}
+
+	void MoveTarget(Vector2 displacement)
+    {
+		if (gameObject.tag == "Player" || gameObject.tag == "Enemy" || gameObject.tag == "Ally")
+		{
 			Movement movement = GetComponent<Movement>();
 			if (movement)
 			{
 				movement.Move(displacement, Vector2.zero);
-			} else
-            {
+			}
+			else
+			{
 				Debug.LogError("gameObject requires movement script if passenger");
-            }
-		} else
+			}
+		}
+		else
 		{
 			transform.Translate(displacement);
 		}
